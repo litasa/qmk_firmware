a01410e3f1a7f9eecec8d75fbd6720114b8f6aad
Jakob Törmä <jakob@torma.nu>
2020-07-11 14:29:13 +0200

    added my keyboard layout

---
+++ keyboards/ergodox_ez/keymaps/litasa/config.h
@@ -0,0 +1,8 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#define AUTO_SHIFT_TIMEOUT 200
+#define NO_AUTO_SHIFT_SPECIAL
+#define NO_AUTO_SHIFT_NUMERIC
